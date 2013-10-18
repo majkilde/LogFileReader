dbcac370e5f05b460d4c05644d1f54f2fec3c71a
Jakob Majkilde <jakob@majkilde.dk>
2013-10-19 00:38:09 +0200

    backup

--- nsf/Code/Java/dk/majkilde/logreader/beans/LogFilesBean.java
+++ nsf/Code/Java/dk/majkilde/logreader/beans/LogFilesBean.java
@@ -9,10 +9,13 @@ import dk.majkilde.logreader.files.FileManager;
 import dk.xpages.log.LogManager;
 import dk.xpages.log.Logger;
 import dk.xpages.utils.NotesStrings;
+import dk.xpages.utils.XSPUtils;
 
 public class LogFilesBean implements Serializable {
 	private final Logger log = LogManager.getLogger();
 	private final FileManager manager = new FileManager("logfiles");
+
+	//	private IFile currentFile = null;
 
 	public LogFilesBean() {
 		log.info("Bean initialized: {0}", LogFilesBean.class.getName());
@@ -39,4 +42,30 @@ public class LogFilesBean implements Serializable {
 	public Collection<FileList> getFilelists() {
 		return manager.getFilelists();
 	}
+
+	public FileList getFirstFilelist() {
+		return getFilelists().iterator().next();
+	}
+
+	public FileList getCurrentFilelist() {
+		String menu = XSPUtils.getUrlParameter("menu");
+		FileList result = manager.getFilelist(menu);
+
+		if (result == null) {
+			return getFirstFilelist();
+		} else {
+			return result;
+		}
+	}
+
+	//	public void setCurrentFile(IFile file) {
+	//		this.currentFile = file;
+	//	}
+	//
+	//	public IFile getCurrentFile() {
+	//		if (currentFile == null) {
+	//			currentFile = getCurrentFilelist().getNewest();
+	//		}
+	//		return currentFile;
+	//	}
 }
--- nsf/Code/Java/dk/majkilde/logreader/files/FileList.java
+++ nsf/Code/Java/dk/majkilde/logreader/files/FileList.java
@@ -1,5 +1,6 @@
 package dk.majkilde.logreader.files;
 
+import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,10 +8,12 @@ import java.util.List;
 
 import dk.xpages.log.LogManager;
 import dk.xpages.log.Logger;
+import dk.xpages.utils.NotesStrings;
 
 public class FileList implements Serializable {
 
 	private final Logger log = LogManager.getLogger();
+	private IFile current = null;
 
 	/**
 	 * 
@@ -37,6 +40,10 @@ public class FileList implements Serializable {
 		return Directory.getCleanPattern(pattern);
 	}
 
+	public String getPath() {
+		return NotesStrings.strLeftBack(getPattern(), File.separator);
+	}
+
 	public IFile getNewest() {
 		if (files.size() > 0) {
 			return files.get(0);
@@ -58,5 +65,16 @@ public class FileList implements Serializable {
 
 	public int getCount() {
 		return files.size();
+	}
+
+	public void setCurrent(IFile current) {
+		this.current = current;
+	}
+
+	public IFile getCurrent() {
+		if (current == null) {
+			current = getNewest();
+		}
+		return current;
 	}
 }
