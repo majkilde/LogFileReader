1262d76a6d06afb5cdbaa12b6ad57f634dcfb8dd
Jakob Majkilde <jakob@majkilde.dk>
2013-12-15 17:33:47 +0100

    canReadLog method included (with the restricted=true in config.xml)

--- nsf/Code/Java/dk/majkilde/logreader/menu/MenuItem.java
+++ nsf/Code/Java/dk/majkilde/logreader/menu/MenuItem.java
@@ -6,8 +6,13 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import lotus.domino.NotesException;
+import lotus.domino.Session;
+import lotus.notes.addins.DominoServer;
+import lotus.notes.addins.ServerAccess;
 import dk.majkilde.logreader.menu.actions.IAction;
 import dk.majkilde.logreader.menu.actions.NullAction;
+import dk.xpages.utils.NotesObjects;
 import dk.xpages.utils.NotesStrings;
 import dk.xpages.utils.XML;
 
@@ -21,6 +26,8 @@ public class MenuItem implements IMenu, Serializable {
 
 	private final String name;
 	private final String id;
+	private boolean restricted = false;
+
 	private IAction action = new NullAction();
 
 	private MenuItem(final String name, final String id) {
@@ -49,6 +56,8 @@ public class MenuItem implements IMenu, Serializable {
 			menu = new MenuItem(menuXML.string("name"), menuXML.string("id"));
 		}
 
+		menu.setRestricted(menuXML.string("restricted").equalsIgnoreCase("true"));
+
 		menu.setAction(ActionFactory.createAction(menuXML, menu));
 
 		for (XML itemXML : menuXML.children("item")) {
@@ -56,6 +65,10 @@ public class MenuItem implements IMenu, Serializable {
 			menu.add(sub);
 		}
 		return menu;
+	}
+
+	public void setRestricted(boolean restricted) {
+		this.restricted = restricted;
 	}
 
 	public String getName() {
@@ -94,9 +107,36 @@ public class MenuItem implements IMenu, Serializable {
 
 	public boolean isVisible() {
 		try {
-			return getAction().isValid();
+			if (restricted) {
+				return getAction().isValid() && canReadLogs(NotesObjects.getCurrentSession());
+			} else {
+				return getAction().isValid();
+			}
 		} catch (Exception e) {
 			return false;
 		}
 	}
+
+	private boolean canReadLogs(final Session session) {
+
+		boolean result = false;
+
+		try {
+			String username = session.getEffectiveUserName();
+
+			DominoServer server = new DominoServer();
+
+			result = server.checkServerAccess(username, ServerAccess.PROG_UNRESTRICTED);
+
+			result = result || server.checkServerAccess(username, ServerAccess.VIEW_ONLY_ADMIN);
+
+		} catch (NotesException ne) {
+
+			ne.printStackTrace();
+
+		}
+		return result;
+
+	}
+
 }
